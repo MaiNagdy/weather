4d0371c403182baa2464cab759776507c77dfc24
MaiNagdy <43970241+MaiNagdy@users.noreply.github.com>
2020-02-07 00:43:56 +0200

    Update cairo.py

--- cairo.py
+++ cairo.py
@@ -14,7 +14,7 @@ def weather():
  api_address='22dc9f732f316339d81e20d9a93a8866'
  url ='http://api.openweathermap.org/data/2.5/weather?appid={}&q={}'.format(api_address,q)
  response =(requests.get(url).json())
- return response["weather"]
+ return response
  
 
  
